3193c93de860649eddb530eaeaacbbef27aea499
mustafaboz <mustafaegebozkaya555@hotmail.com>
2022-05-15 23:55:44 +0300

    edited penguins eda app

--- penguin_app/penguin_EDA.py
+++ penguin_app/penguin_EDA.py
@@ -21,47 +21,67 @@ class Penguin_EDA:
     def initapp(self):
         # title of the app
         st.title("Penguins species Exploration Data Analysis")
-        st.header("Created by Mustafa Bozkaya")  # header of the app
+        # st.header("Created by Mustafa Bozkaya")  # header of the app
         # subheader of the app
         st.subheader(
             "This is a simple program to explore the penguin species data")
 
-        st.write("penguins data Information")  # write the data information
+        st.write("General  Data Information")  # write the data information
         # write the shape of the data
         st.success("The penguins data contains \n " + str(self.data_shape[0]) +
                    " rows and " + str(self.data_shape[1]) + " columns")  # write the shape of the data
         # write the data columns
-        st.write("The penguins data contains the following columns:")
-        st.write(self.data_columns)  # write the data columns
+        st.markdown("<h3>Data Columns is below</h3>", unsafe_allow_html=True)
+        st.write(self.data_columns.T)  # write the data columns
 
         # create side bar menu for data visualization
-        st.sidebar.title("Penguins Data Analysis Tools")  # sidebar title
-        # sidebar options
-        sidebar_option = st.sidebar.selectbox(
-            "Please select an option", ("Show data", "Show data types",
-                                        "Show data summary", "Show data visualization", "Show data information"))  # select an option from the sidebar
+        st.sidebar.title("Data Analysis Tools")  # sidebar title
 
-        if sidebar_option == "Show data Top 10":
+        # sidebar options
+        # sidebar_option = st.sidebar.selectbox(
+        #     "Please select an option", ("Show data Top 10", "Show data types",
+        #                                 "Show data summary", "Show data visualization", "Show data information"))  # select an option from the sidebar
+
+        if st.sidebar.checkbox("Show data Top 10"):  # checkbox for data top 10
             st.dataframe(self.data.head(10))
-        elif sidebar_option == "Show data types":
-            st.write(self.data_types)
-        elif sidebar_option == "Show data summary":
+        if st.sidebar.checkbox("Show data types"):  # checkbox for data types
+            print("dtata types info :", type(self.data_types))
+            # self.data_types_df = pd.DataFrame(self.data_types)
+            # st.dataframe(self.data_types_df.style.highlight_max(axis=0))
+            data_types_df = self.numeric_exp(self.data)
+            st.write(data_types_df)
+        if st.sidebar.checkbox("Show data summary"):  # checkbox for data summary
             # write the data descriptive statistics (mean, std, min, max, 25%, 50%, 75%) for each column
             st.write("The penguins data descriptive statistics")
             st.write(self.data.describe())
             # self.data.info()  # write the data information (number of rows, number of columns, data types, data memory size, data memory usage, data memory usage percent, data memory usage percent relative to the total physical memory, data memory usage percent relative to the total swap memory, data memory usage percent relative to the total virtual memory)
 
-        elif sidebar_option == "Show data information":
+        # checkbox for data visualization
+        if st.sidebar.checkbox("Show data information"):
             buffer = io.StringIO()  # create a buffer object to store the dataframe information
-            self.data.info(buf=buffer)
-            s = buffer.getvalue()  # get the dataframe information from the buffer object
-            # split the dataframe information into a list
-            s = s.split('\n')
-            # write the data information
-            st.write("The penguins data information")
-            st.write(s)  # write the data information
+            self.data.info(buf=buffer)  # write the data information (number of rows, number of columns, data types, data memory size, data memory usage, data memory usage percent, data memory usage percent relative to the total physical memory, data memory usage percent relative to the total swap memory, data memory usage percent relative to the total virtual memory) to the buffer object and then write the buffer object
 
-        elif sidebar_option == "Show data visualization":
+            info_str = buffer.getvalue()  # get the dataframe information from the buffer object
+            # split the dataframe information into a list
+            # str = s.split('\n')
+
+            info_df = pd.DataFrame([line.split()
+                                   for line in info_str.split('\n')[3:-3]])
+
+            # drop the column  index
+            info_df.drop(info_df.columns[0], axis=1, inplace=True)
+
+            info_df.columns = ['Columns', 'Not Null',
+                               'Count', 'Dtype']  # rename the columns
+            # drop index row
+            info_df.drop(info_df.index[1], inplace=True)
+            # reset the index of the dataframe and drop the old index
+            info_df.reset_index(drop=True, inplace=True)
+            # drop the first row of the dataframe (the dataframe information) and drop the old index of the dataframe
+
+            st.write(info_df)
+        # checkbox for data information
+        if st.sidebar.checkbox("Show data visualization "):
             st.write("The penguins data visualization")
             # create a subplot
             fig, ax = plt.subplots(figsize=(10, 10))
@@ -73,11 +93,11 @@ class Penguin_EDA:
         species_data = self.data["species"].unique().tolist()
         species_data.insert(0, "all")
         with st.spinner('Wait for it...'):
-            time.sleep(5)
+            time.sleep(2)
 
             st.success(f"penguıns species: {species_data}")
 
-        check_viz = st.sidebar.checkbox("Show data visualization")
+        check_viz = st.sidebar.checkbox("Show data visualization with Seaborn")
         if check_viz:
 
             selected_species = st.sidebar.selectbox("Select a species to visualize",
@@ -124,6 +144,52 @@ class Penguin_EDA:
                 st.bar_chart(
                     self.data[self.data["species"] == selected_species]["species"].value_counts())
                 st.pyplot()
+            # create markdown for copyright information and link to the source code repository of the app on GitHub
+
+        st.markdown("""
+        **Created by :** [Mustafa Bozkaya](https://www.linkedin.com/in/mustafa-bozkaya)
+        **Data source:** [Penguins species data](https://www.kaggle.com/datasets/mustafabozka/palmers-penguins)
+        **Code source:** [GitHub](https://github.com/ds-ml-dl-ai-solutions/projects/blob/main/penguin_app/penguin_EDA.py)
+        """)
+
+    def numeric_exp(self, data):
+
+        df_types = pd.DataFrame(data.dtypes, columns=['Data Type'])
+
+        numerical_cols = df_types[~df_types['Data Type'].isin(['object',
+                                                               'bool'])].index.values
+
+        st.success(f"numeric columns : {numerical_cols}")
+        df_types['Count'] = data.count()
+        df_types['Unique Values'] = data.nunique()
+        st.warning(data.nunique())
+        df_types['Min'] = data[numerical_cols].min()
+        st.warning(data[numerical_cols].min())
+        df_types['Max'] = data[numerical_cols].max()
+        st.warning(data[numerical_cols].max())
+        df_types['Average'] = data[numerical_cols].mean()
+        st.warning(data[numerical_cols].mean())
+        df_types['Median'] = data[numerical_cols].median()
+        st.warning(data[numerical_cols].median())
+        df_types['St. Dev.'] = data[numerical_cols].std()
+        st.warning(data[numerical_cols].std())
+
+        return df_types.astype(str)
+
+    def infoOut(data, details=False):
+        dfInfo = data.columns.to_frame(name='Column')
+        dfInfo['Non-Null Count'] = data.notna().sum()
+        dfInfo['Dtype'] = data.dtypes
+        dfInfo.reset_index(drop=True, inplace=True)
+        if details:
+            rangeIndex = (dfInfo['Non-Null Count'].min(),
+                          dfInfo['Non-Null Count'].min())
+            totalColumns = dfInfo['Column'].count()
+            dtypesCount = dfInfo['Dtype'].value_counts()
+            totalMemory = dfInfo.memory_usage().sum()
+            return dfInfo, rangeIndex, totalColumns, dtypesCount, totalMemory
+        else:
+            return dfInfo
 
 
 if __name__ == "__main__":
